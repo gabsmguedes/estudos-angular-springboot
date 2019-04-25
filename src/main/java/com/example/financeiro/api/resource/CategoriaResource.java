d273ff426f70e3aad567b08fdbc876fe75ebc69e
gabsmguedes <ga.mouraguedes@gmail.com>
2019-04-25 19:28:40 -0300

    Criando Insert utilizando POST e GET para buscar por id

--- src/main/java/com/example/financeiro/api/resource/CategoriaResource.java
+++ src/main/java/com/example/financeiro/api/resource/CategoriaResource.java
@@ -1,11 +1,22 @@
 package com.example.financeiro.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.financeiro.api.model.Categoria;
 import com.example.financeiro.api.repository.CategoriaRepository;
@@ -18,8 +29,23 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping()
-	public List<Categoria> lista(){
+	public List<Categoria> listAll(){
 		return categoriaRepository.findAll();
 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> insert(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
 
+	@GetMapping("/{id}")
+	public Optional<Categoria> findById(@PathVariable Long id) {
+		return categoriaRepository.findById(id);
+	}
 }
