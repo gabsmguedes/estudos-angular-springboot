758cff106d6ff2f6f2a48fc107ae2b704f7230db
gabsmguedes <ga.mouraguedes@gmail.com>
2019-04-26 16:30:26 -0300

    Validando valores invalidos com Bean Validation

--- src/main/java/com/example/financeiro/api/exceptionhandler/FinanceiroExceptionHandler.java
+++ src/main/java/com/example/financeiro/api/exceptionhandler/FinanceiroExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.example.financeiro.api.exceptionhandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -7,28 +11,50 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class FinanceiroExceptionHandler extends ResponseEntityExceptionHandler{
+public class FinanceiroExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Autowired
 	private MessageSource messageSource;
-	
+
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
 		String mensagemDesenvolvedor = ex.getCause().toString();
-		return handleExceptionInternal(ex, new Error(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+		List<Error> errors = Arrays.asList(new Error(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
 	}
-	
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<Error> errors = criaListaDeErros(ex.getBindingResult());
+		return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	private List<Error> criaListaDeErros(BindingResult bindingResult) {
+		List<Error> erros = new ArrayList<>();
+
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = fieldError.toString();
+			erros.add(new Error(mensagemUsuario, mensagemDesenvolvedor));
+		}
+		return erros;
+	}
+
 	public static class Error {
 		private String mensagemUsuario;
 		private String mensagemDesenvolvedor;
-		
+
 		public Error(String mensagemUsuario, String mensagemDesenvolvedor) {
 			super();
 			this.mensagemUsuario = mensagemUsuario;
