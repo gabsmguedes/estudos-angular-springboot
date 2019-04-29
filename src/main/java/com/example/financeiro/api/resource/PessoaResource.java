a4ffb40140148a7f0c45fee4da83102da7107325
gabsmguedes <ga.mouraguedes@gmail.com>
2019-04-29 16:50:28 -0300

    Atualizando pessoa com PUT

--- src/main/java/com/example/financeiro/api/resource/PessoaResource.java
+++ src/main/java/com/example/financeiro/api/resource/PessoaResource.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -59,5 +61,14 @@ public class PessoaResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		pessoaRepository.deleteById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Pessoa> update(@PathVariable Long id, @Valid @RequestBody Pessoa pessoa) throws Exception{
+		Pessoa pessoaSalva = pessoaRepository.findById(id).get();
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "id");
+		//Pessoa pessoaAlterada = pessoaSalva.orElseThrow(() -> new Exception());
+		pessoaRepository.save(pessoaSalva);
+		return ResponseEntity.ok(pessoaSalva);
 	}
 }
