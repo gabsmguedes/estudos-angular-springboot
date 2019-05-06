b966ed415638705109d0f6b4f289bf28526d5769
gabsmguedes <ga.mouraguedes@gmail.com>
2019-05-06 17:32:05 -0300

    Adicionado Refresh token e JWT

--- src/main/java/com/example/financeiro/api/config/AutorizationServerConfig.java
+++ src/main/java/com/example/financeiro/api/config/AutorizationServerConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -20,17 +21,28 @@ public class AutorizationServerConfig extends AuthorizationServerConfigurerAdapt
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("angular").secret("{noop}angular").scopes("read", "write").authorizedGrantTypes("password")
-		.accessTokenValiditySeconds(1800);
+		clients.inMemory().withClient("angular").secret("{noop}angular").scopes("read", "write")
+		.authorizedGrantTypes("password", "refresh_token")
+		.accessTokenValiditySeconds(20).refreshTokenValiditySeconds(3600*24);
 	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+		endpoints.tokenStore(tokenStore())
+		.accessTokenConverter(accessTokenConverter())
+		.reuseRefreshTokens(false)
+		.authenticationManager(authenticationManager);
 	}
 	
 	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("financeiro");
+		return accessTokenConverter;
+	}
+
+	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 }
